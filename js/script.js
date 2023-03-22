46cf95982c6ea366dae89a08644821a33fe5a7aa
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-22 15:19:24 +0100

    Project scaffolding and JS to-do in human language

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/** 
+ * [] Creare un array:
+ *  [] un oggetto per ogni persona con nome, cognome, ruolo e foto
+ *  [] stampare su console le informazioni di ogni persona (per la foto stampare stringa)
+ *  [] stampare informazioni su dom sotto forma di stringa
+ *  [] (bonus) trasfromare la foto da stringa a immagine
+ *  [] (bonus) organizzare i singoli membri in card
+ */
