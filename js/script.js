b62467f8353e11de9b1e0f8aadf291cbfa6dae68
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-22 18:05:45 +0100

    Bonus 1

--- js/script.js
+++ js/script.js
@@ -47,9 +47,18 @@ const peopleArray = [
 ]
 
 const listElem = document.querySelector("ul");
+let listContent = '';
 
-for (let i = 0; i <= peopleArray.length; i++) {
-    console.log(peopleArray[i].name, peopleArray[i].lastName, peopleArray[i].job, peopleArray[i].photo);
-    listElem.innerHTML += `<li>${peopleArray[i].name} ${peopleArray[i].lastName}, ${peopleArray[i].job}, ${peopleArray[i].photo}</li>`
+for (let i = 0; i < peopleArray.length; i++) {
+    console.log(peopleArray[i]);
+    listContent += `
+    <li>
+    <img src="img/${peopleArray[i].photo}" alt="official photo">
+    <h3>${peopleArray[i].name} ${peopleArray[i].lastName}</h3>
+    <div class="role">${peopleArray[i].job}</div>
+    </li>
+    `
 };
+
+listElem.innerHTML = listContent;
 
